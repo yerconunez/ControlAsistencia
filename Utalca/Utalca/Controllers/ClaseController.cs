9f4a59eeb4d01dcebf8a626c7d9734277a8af01f
cgalvezv <camilogalvezv@gmail.com>
2016-06-15 19:50:26 -0400

    Avance 15/06/2016

---
+++ Utalca/Utalca/Controllers/ClaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Utalca.Controllers
+{
+    public class ClaseController : Controller
+    {
+        // GET: Clase
+        public ActionResult Index(long idCurso, DateTime fechaClase)
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var curso = servicio.Curso(idCurso);
+            foreach(var clase in curso.Horario)
+            {
+                if (clase.Fecha.Equals(fechaClase)) { return View(clase); }
+            }
+            return View();
+        }
+    }
+}
---
+++ Utalca/Utalca/Controllers/CursoApiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Utalca.Controllers
+{
+    public class CursoApiController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Utalca.ControlAsistencia.Curso> Get()
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var cursos = servicio.Cursos();
+            return cursos;
+        }
+
+        // GET api/<controller>/5
+        public Utalca.ControlAsistencia.Curso Get(int id)
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var curso = servicio.Curso(id);
+            return curso;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
